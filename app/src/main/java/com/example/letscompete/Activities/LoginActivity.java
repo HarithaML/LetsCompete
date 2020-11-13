42872c1d2dd1dc46883eb6006935a4e6cbe20521
HarithaML <lakshmi.haritha@outlook.com>
2020-11-12 16:18:07 -0800

    StarterCode

---
+++ app/src/main/java/com/example/letscompete/Activities/LoginActivity.java
@@ -0,0 +1,301 @@
+package com.example.letscompete.Activities;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.letscompete.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 100;
+    GoogleSignInClient mGoogleSignInClient;
+    //views
+    EditText mEmailEt, mPasswordEt;
+    TextView notHaveAcccountTv, mRecoverPassTv;
+    Button mLoginBtn;
+    SignInButton mGoogleLoginBtn;
+    Toast t;
+
+    // Declare an Instance of FireBaseAuth
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mGoogleLoginBtn = findViewById(R.id.googleLoginBtn);
+
+        // action bar ad its title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        // enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        //before mAuth
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+        //init
+        mEmailEt = findViewById(R.id.emailEt);
+        mPasswordEt = findViewById(R.id.paswordEt);
+        notHaveAcccountTv = findViewById(R.id.notHave_accountTv);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mRecoverPassTv = findViewById(R.id.recoverPassTv);
+        //login button click
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //input data
+                String email = mEmailEt.getText().toString();
+                String passw = mPasswordEt.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    // set error and focus to email editText
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                } else {
+                    t = Toast.makeText(LoginActivity.this, "Logging in the User",
+                                       Toast.LENGTH_LONG);
+                    loginUser(email, passw);
+                }
+            }
+        });
+        //not have account textview click
+        notHaveAcccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        mRecoverPassTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
+
+        //handle google login btn click
+        mGoogleLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            }
+        });
+    }
+
+    private void showRecoverPasswordDialog() {
+        //AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+        //set layout
+        LinearLayout linearLayout = new LinearLayout(this);
+        //views to set in dialog
+        EditText emailEt = new EditText(this);
+        emailEt.setHint("Email");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        /*sets the min width of a editview to fit a text of 'N' letters regardless of the actual text extension and text size*/
+        emailEt.setMinEms(16);
+
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(10, 10, 10, 10);
+        builder.setView(linearLayout);
+
+
+        //buttons recover
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email = emailEt.getText().toString().trim();
+                beginRecovery(email);
+            }
+        });
+        //buttons cancel
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dismiss dialog
+                dialog.dismiss();
+            }
+        });
+
+        //show dialog
+        builder.create().show();
+
+    }
+
+    private void beginRecovery(String email) {
+        Log.i(TAG, "Sending email");
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Email Sent ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Failed.... ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //get and show proper error message
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginUser(String email, String passw) {
+        //email and password pattern is vallid ,show toast
+        t.show();
+        mAuth.signInWithEmailAndPassword(email, passw)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Logging in");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this,
+                                           "Authentication Succcess.",
+                                           Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, DashBoardActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "Login:failure", task.getException());
+                            Toast.makeText(LoginActivity.this,
+                                           "Authentication failed.",
+                                           Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // error dismiss toast
+                t = Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();// go previous activity
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Get user email and uid from auth
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+
+                            // When user is registered store user info in firebase realtime database too
+                            //using HashMap
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            //put info in hashMap
+                            hashMap.put("email",email);
+                            hashMap.put("uid",uid);
+                            hashMap.put("name","");// will add later e.g. edit profile
+                            hashMap.put("phone","");// will add later e.g. edit profile
+                            hashMap.put("image","");// will add later e.g. edit profile
+                            // Firebase database instance
+                            FirebaseDatabase databse = FirebaseDatabase.getInstance();
+                            //path to store user data named "Users"
+                            DatabaseReference reference = databse.getReference("Users");
+                            //put data within hashmap in database
+                            reference.child(uid).setValue(hashMap);
+
+
+                            // go to profile activity
+                            startActivity(new Intent(LoginActivity.this, DashBoardActivity.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT)
+                                    .show();
+
+                        }
+
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // get and show error message
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/letscompete/Activities/RegisterActivity.java
@@ -0,0 +1,147 @@
+package com.example.letscompete.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.letscompete.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText mEmailEt,mPasswordEt;
+    Button mRegisterBtn;
+    TextView mHaveAccountTv;
+    private static final String TAG = "RegisterActivity";
+    Toast t ;
+    // Declare an Instance of FireBaseAuth
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        // action bar ad its title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        // enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        //init
+        mEmailEt = findViewById(R.id.emailEt);
+        mPasswordEt= findViewById(R.id.paswordEt);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mHaveAccountTv = findViewById(R.id.have_accountTv);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPasswordEt.getText().toString().trim();
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    // set error and focus to email editText
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                }else if(password.length()<6){
+                    // set error and focus to password editText
+                    mPasswordEt.setError("Password length less than 6 characters");
+                    mPasswordEt.setFocusable(true);
+                }else{
+                    t = Toast.makeText(RegisterActivity.this, "Registering User",
+                                       Toast.LENGTH_LONG);
+                    registerUser(email,password);
+                }
+            }
+        });
+        // handle login textView click listener
+        mHaveAccountTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+    private void registerUser(String email,String password){
+        //email and password pattern is vallid ,show toast
+        t.show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Get user email and uid from auth
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+
+                            // When user is registered store user info in firebase realtime database too
+                            //using HashMap
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            //put info in hashMap
+                            hashMap.put("email",email);
+                            hashMap.put("uid",uid);
+                            hashMap.put("name","");// will add later e.g. edit profile
+                            hashMap.put("phone","");// will add later e.g. edit profile
+                            hashMap.put("image","");// will add later e.g. edit profile
+                            // Firebase database instance
+                            FirebaseDatabase databse = FirebaseDatabase.getInstance();
+                            //path to store user data named "Users"
+                            DatabaseReference reference = databse.getReference("Users");
+                            //put data within hashmap in database
+                            reference.child(uid).setValue(hashMap);
+
+                            Toast.makeText(RegisterActivity.this,
+                                           "Authentication Succcess.",
+                                           Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, DashBoardActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this,
+                                           "Authentication failed.",
+                                           Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // error dismiss toast
+                        t = Toast.makeText(RegisterActivity.this,"Authentication Failed",Toast.LENGTH_SHORT);
+                    }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();// go previous activity
+        return super.onSupportNavigateUp();
+    }
+
+
+
+}
