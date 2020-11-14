e638a37d19f93ae545f72d21de95cab96479bf95
HarithaML <lakshmi.haritha@outlook.com>
2020-11-14 10:16:05 -0800

    resolve minor issues,make package names lower case

---
+++ app/src/main/java/com/example/letscompete/fragments/ContactsFragment.java
@@ -0,0 +1,252 @@
+package com.example.letscompete.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.example.letscompete.activities.MainActivity;
+import com.example.letscompete.adapters.AdapterUsers;
+import com.example.letscompete.models.ModelUser;
+import com.example.letscompete.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContactsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactsFragment extends Fragment {
+    RecyclerView recyclerView;
+    AdapterUsers adapterUsers;
+    List<ModelUser> userList;
+    FirebaseAuth firebaseAuth;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment UsersFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactsFragment newInstance(String param1, String param2) {
+        ContactsFragment fragment = new ContactsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        //init recyclerView
+        recyclerView= view.findViewById(R.id.users_recyclerView);
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //set its properties
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //init user list
+        userList = new ArrayList<>();
+
+        //get all users
+        getAllUsers();
+
+        return view;
+    }
+
+    private void getAllUsers() {
+        //get current user
+        FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
+        //get path of database named "Users" containing users info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for(DataSnapshot ds:  snapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                    //get all users except currently signed in user
+                    if(!modelUser.getUid().equals(fuser.getUid())){
+                        userList.add(modelUser);
+                    }
+                    System.out.println(userList);
+                    //adapter
+                    adapterUsers = new AdapterUsers(getActivity(),userList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+    private void searchUsers(String query) {
+        //get current user
+        FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
+        //get path of database named "Users" containing users info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for(DataSnapshot ds:  snapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                    //get all searched users except currently signed in user
+                    if(!modelUser.getUid().equals(fuser.getUid())){
+                        if(modelUser.getName().toLowerCase().contains(query.toLowerCase())||modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
+                            userList.add(modelUser);
+                        }
+
+                    }
+                    System.out.println(userList);
+                    //adapter
+                    adapterUsers = new AdapterUsers(getActivity(),userList);
+                    adapterUsers.notifyDataSetChanged();
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //inflating menu
+        inflater.inflate(R.menu.menu_main,menu);
+        //SearchView
+        MenuItem item = menu.findItem(R.id.action_Search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //called when user presses search button from keyboard
+                if(!TextUtils.isEmpty(query.trim())){
+                    //search text contains text, search it
+                    searchUsers(query);
+                }else{
+                    getAllUsers();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //called when user presses any single letter
+                if(!TextUtils.isEmpty(newText.trim())){
+                    //search text contains text, search it
+                    searchUsers(newText);
+                }else{
+                    getAllUsers();
+                }
+
+
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+
+    /* handle menu item clicks*/
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id= item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user!=null){
+            // user is signed in stay here
+            //set email of logged in user
+//            mProfileTv.setText(user.getEmail());
+        }else{
+            // user not signed in go to main activity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/letscompete/fragments/HomeFragment.java
@@ -0,0 +1,112 @@
+package com.example.letscompete.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.letscompete.activities.MainActivity;
+import com.example.letscompete.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    FirebaseAuth firebaseAuth;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        return view;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //inflating menu
+        inflater.inflate(R.menu.menu_main,menu);
+        menu.findItem(R.id.action_Search).setVisible(false);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+    /* handle menu item clicks*/
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id= item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user!=null){
+            // user is signed in stay here
+            //set email of logged in user
+//            mProfileTv.setText(user.getEmail());
+        }else{
+            // user not signed in go to main activity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
+    }
+}
