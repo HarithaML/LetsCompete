d9b3a96ba8abd7428b91cc23654dab4e9320337c
harikasushmak <harikasushma96@gmail.com>
2020-11-21 00:10:46 +0530

    Layout changes

--- app/src/main/java/com/example/letscompete/activities/MainActivity.java
+++ app/src/main/java/com/example/letscompete/activities/MainActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.example.letscompete.R;
 
 public class MainActivity extends AppCompatActivity {
     Button mRegisterBtn,mLoginBtn;
+    ViewFlipper v_flipper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +47,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+
+
+        int images [] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4,R.drawable.slide5,R.drawable.slide6};
+
+        v_flipper = findViewById(R.id.logo);
+
+
+        for (int image:images){
+            filpperImages(image);
+        }
     }
+
+    public void filpperImages (int image) {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(3000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+
+
+
+
     public void something() {
         Intent intent = new Intent(this, Setting_Activity.class);
         startActivity(intent);
