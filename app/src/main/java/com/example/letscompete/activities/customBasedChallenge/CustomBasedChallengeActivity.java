eeb2fc2b1948cef4ae1c6cd519e5b321b1a1ea74
HarithaML <Lakshmi.haritha@outlook.com>
2020-11-25 18:58:19 -0800

    added packages to challenges

---
+++ app/src/main/java/com/example/letscompete/activities/customBasedChallenge/CustomBasedChallengeActivity.java
@@ -0,0 +1,16 @@
+package com.example.letscompete.activities.customBasedChallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.letscompete.R;
+
+public class CustomBasedChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_based_challenge);
+    }
+}
